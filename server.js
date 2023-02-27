99e6c40a5b3f1fc4cc7f39a39e6deb93aeb52231
Sumeyye Yegen <sumeyyeyegen746@gmail.com>
2023-02-27 15:45:14 +0300

    the error part completed.

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = require("./app");
 
+process.on("uncaughtException",(err) =>{
+  console.log(err); 
+  console.log('UncaughtException shutting down application ');
+    process.exit(1);
+} )
+
+
 dotenv.config({ path: "./config.env" });
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
@@ -17,44 +24,19 @@ mongoose
   .then((con) => {
     // console.log(con.connection);
     console.log("DB Connection Successfully");
-  });
-// console.log(app.get("env"));
-// console.log(process.env);
-
-// const nftSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: [true, "A NFT must have a name"],
-//     unique: true,
-//   },
-//   rating: {
-//     type: Number,
-//     default: 4.5,
-//   },
-//   price: {
-//     type: Number,
-//     required: [true, "A NFT must have price"],
-//   },
-// });
-
-// const testNFT = new NFT({
-//   name: "The Daulat Monkey",
-//   rating: 3.2,
-//   price: 567,
-// });
-
-// testNFT
-//   .save()
-//   .then((docNFT) => {
-//     console.log(docNFT);
-//   })
-//   .catch((error) => {
-//     console.log("ERROR:", error);
-//   });
-
-// const NFT = mongoose.model("NFT", nftSchema);
+  })
+  // .catch(err => console.log('ERROR'))
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
+  console.log('Mode : ', process.env.NODE_ENV);
   console.log(`App running on port ${port}....`);
 });
+
+process.on("unhandledRejection",err =>{
+  console.log(err.name,err.message); 
+  console.log('Unhandlerejection shutting down application ');
+  server.close(() =>{
+    process.exit(1);
+  })
+}) 
